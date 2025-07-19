bdba87cb0582bbe1259902645091e4c7abb119fc
Daniel McPherson <daniel.mcpherson@live.co.uk>
2025-07-19 21:08:50 +0100

    Added keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -270,6 +270,7 @@ vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code action
 vim.keymap.set('n', '<C-e>', vim.diagnostic.open_float, { desc = 'Show error message' })
 vim.keymap.set('n', '<leader>ne', vim.diagnostic.goto_next, { desc = 'Show next error message' })
 vim.keymap.set('n', '<leader>pe', vim.diagnostic.goto_prev, { desc = 'Show previous error message' })
+vim.keymap.set('n', '<leader>pe', vim.diagnostic.goto_prev, { desc = 'Show previous error message' })
 
 
 -- Remaps to use the system clipboard
@@ -281,3 +282,26 @@ vim.keymap.set('n', '<leader>pe', vim.diagnostic.goto_prev, { desc = 'Show previ
 -- vim.keymap.set('n', 'P', '"+P')
 -- vim.keymap.set('n', 'd', '"+d')
 -- vim.keymap.set('v', 'd', '"+d')
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "p", "\"_dP")
+vim.keymap.set("v", "d", "\"_d")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ts<CR>")
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
